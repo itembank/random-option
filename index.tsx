576c1063e8cdc9d5177c46d43060eead4079e5a0
calcuis <113646141+calcuis@users.noreply.github.com>
2023-08-05 09:42:56 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -52,7 +52,8 @@ function App() {
                     "c": "option 3.",
                     "d": "option 4.",
                     "ans": "a"
-                }]
+                }
+  		]
 
   const [item, setItem] = useState<any>([]);
   const [swap, setSwap] = useState(0); //For #0
@@ -102,30 +103,50 @@ function App() {
             <li>{item.title&&item.title}</li>
 
             {item.b&&!item.c&&<>
-            {item.a&&<A item={item} values={values}/>}
-            {item.b&&<B item={item} values={values}/>}
+	            {item.a&&<A item={item} values={values}/>}
+	            {item.b&&<B item={item} values={values}/>}
             </>}
 
             {(item.c=="all of the above."||item.d=="all of the above.")&&<>
-            {item.a&&((swap==1||(item.e&&swap==4))?<B item={item} values={values}/>:(((item.d&&swap==2)||(item.e&&swap==5))?<C item={item} values={values}/>:((item.e&&swap==6)?<D item={item} values={values}/>:<A item={item} values={values}/>)))}
-            {item.b&&((swap==1||(item.e&&swap==4))?<A item={item} values={values}/>:((item.e&&swap==5)?<D item={item} values={values}/>:(((item.e&&swap==6)||(item.d&&swap==3))?<C item={item} values={values}/>:<B item={item} values={values}/>)))}
-            {item.c&&(((item.d&&swap==2)||(item.e&&swap==5))?<A item={item} values={values}/>:((item.e&&swap==4)?<D item={item} values={values}/>:(((item.e&&swap==6)||(item.d&&swap==3))?<B item={item} values={values}/>:<C item={item} values={values}/>)))}
-            {item.d&&((item.e&&swap==4)?<C item={item} values={values}/>:((item.e&&swap==5)?<B item={item} values={values}/>:((item.e&&swap==6)?<A item={item} values={values}/>:<D item={item} values={values}/>)))}
-            {item.e&&<E item={item} values={values}/>}
+	            {item.a&&((swap==1||(item.e&&swap==4))?<B item={item} values={values}/>:
+			      (((item.d&&swap==2)||(item.e&&swap==5))?<C item={item} values={values}/>:
+			       ((item.e&&swap==6)?<D item={item} values={values}/>:<A item={item} values={values}/>)))}
+	            {item.b&&((swap==1||(item.e&&swap==4))?<A item={item} values={values}/>:
+			      ((item.e&&swap==5)?<D item={item} values={values}/>:
+			       (((item.e&&swap==6)||(item.d&&swap==3))?<C item={item} values={values}/>:<B item={item} values={values}/>)))}
+	            {item.c&&(((item.d&&swap==2)||(item.e&&swap==5))?<A item={item} values={values}/>:
+			      ((item.e&&swap==4)?<D item={item} values={values}/>:
+			       (((item.e&&swap==6)||(item.d&&swap==3))?<B item={item} values={values}/>:<C item={item} values={values}/>)))}
+	            {item.d&&((item.e&&swap==4)?<C item={item} values={values}/>:
+			      ((item.e&&swap==5)?<B item={item} values={values}/>:
+			       ((item.e&&swap==6)?<A item={item} values={values}/>:<D item={item} values={values}/>)))}
+	            {item.e&&<E item={item} values={values}/>}
             </>}
 
             {item.c&&(!item.d)&&(item.c!=="all of the above.")&&<>
-            {item.a&&(swap==1?<B item={item} values={values}/>:(swap==2?<C item={item} values={values}/>:<A item={item} values={values}/>))}
-            {item.b&&(swap==1?<A item={item} values={values}/>:(swap==3?<C item={item} values={values}/>:<B item={item} values={values}/>))}
-            {item.c&&(swap==2?<A item={item} values={values}/>:(swap==3?<B item={item} values={values}/>:<C item={item} values={values}/>))}
+	            {item.a&&(swap==1?<B item={item} values={values}/>:
+			      (swap==2?<C item={item} values={values}/>:<A item={item} values={values}/>))}
+	            {item.b&&(swap==1?<A item={item} values={values}/>:
+			      (swap==3?<C item={item} values={values}/>:<B item={item} values={values}/>))}
+	            {item.c&&(swap==2?<A item={item} values={values}/>:
+			      (swap==3?<B item={item} values={values}/>:<C item={item} values={values}/>))}
             </>}
 
             {item.d&&(item.d!=="all of the above.")&&<>
-            {item.a&&(swap==4?<B item={item} values={values}/>:(swap==5?<C item={item} values={values}/>:(swap==6?<D item={item} values={values}/>:<A item={item} values={values}/>)))}
-            {item.b&&(swap==4?<A item={item} values={values}/>:(swap==5?<D item={item} values={values}/>:(swap==6?<C item={item} values={values}/>:<B item={item} values={values}/>)))}
-            {item.c&&(swap==4?<D item={item} values={values}/>:(swap==5?<A item={item} values={values}/>:(swap==6?<B item={item} values={values}/>:<C item={item} values={values}/>)))}
-            {item.d&&(swap==4?<C item={item} values={values}/>:(swap==5?<B item={item} values={values}/>:(swap==6?<A item={item} values={values}/>:<D item={item} values={values}/>)))}
-            {item.e&&<E item={item} values={values}/>}
+	            {item.a&&(swap==4?<B item={item} values={values}/>:
+			      (swap==5?<C item={item} values={values}/>:
+			       (swap==6?<D item={item} values={values}/>:<A item={item} values={values}/>)))}
+	            {item.b&&(swap==4?<A item={item} values={values}/>:
+			      (swap==5?<D item={item} values={values}/>:
+			       (swap==6?<C item={item} values={values}/>:<B item={item} values={values}/>)))}
+	            {item.c&&(swap==4?<D item={item} values={values}/>:
+			      (swap==5?<A item={item} values={values}/>:
+			       (swap==6?<B item={item} values={values}/>:<C item={item} values={values}/>)))}
+	            {item.d&&(swap==4?<C item={item} values={values}/>:
+			      (swap==5?<B item={item} values={values}/>:
+			       (swap==6?<A item={item} values={values}/>:<D item={item} values={values}/>)))}
+	            {item.e&&<E item={item} values={values}/>}
+            </>}
 
 	 </ul>
 	</div>
